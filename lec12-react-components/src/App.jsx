85472100d6cbcc7d325c133331e600b8184c227c
Mohammed Naji <m.naji1512@gmail.com>
2026-08-20 15:15:46 +0300

    props and loop

--- lec12-react-components/src/App.jsx
+++ lec12-react-components/src/App.jsx
@@ -1,4 +1,4 @@
-import CSSImage from "./assets/css.png";
+// import CSSImage from "./assets/css.png";
 import Header from "./components/Header";
 import User from "./components/User";
 const users = [
